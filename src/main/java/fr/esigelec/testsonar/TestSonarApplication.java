e44a537e69b28b14349143fbd27c6c10e79ea77e
ccelsin <celsinadjagboni@gmail.com>
2025-02-03 02:08:00 +0100

    third commit

--- src/main/java/fr/esigelec/testsonar/TestSonarApplication.java
+++ src/main/java/fr/esigelec/testsonar/TestSonarApplication.java
@@ -1,4 +1,4 @@
-package fr.esigelec.testSonar;
+package fr.esigelec.testsonar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
