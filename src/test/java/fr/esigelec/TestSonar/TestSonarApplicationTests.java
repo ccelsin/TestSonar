5651ea56fd69cf19610af8948952d2e0ce2a2356
ccelsin <celsinadjagboni@gmail.com>
2025-02-03 01:49:14 +0100

    first commit

---
+++ src/test/java/fr/esigelec/TestSonar/TestSonarApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.esigelec.TestSonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSonarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
